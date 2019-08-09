e67e84bcb0c8f0e09f687e734346c4254a560f49
Paulo Ramos <pauloramos64@gmail.com>
2019-08-09 16:49:41 -0700

    Dashboard

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,10 +1,72 @@
 import React from "react";
-import { container } from "./dashboard.module.css";
+import styles from "./Dashboard.module.css";
+import {
+  formatSidebarDate,
+  getDateOfMasterPayroll,
+  getDateOfMasterPayday,
+  getPriorWorkday,
+  getDateOfMasterTransfer,
+  getDateOfDirectDeposit,
+} from "../../BusinessLogic/functions";
 
-export const Dashboard = () => {
+import { TodayDate } from "../TodayDate/TodayDate";
+import { Search } from "../Search/Search";
+import { ResetButton } from "../ResetButton/ResetButton";
+import { LabelField } from "../LabelField/LabelField";
+
+export const Dashboard = ({
+  selectedDate,
+  today,
+  yearNumber,
+  currentMonth,
+  currentPattern,
+  updateAppState,
+}) => {
   return (
-    <section className={container}>
-      <h1>Dashboard</h1>
+    <section className={styles.container}>
+      <div className={styles.col1}>
+        <TodayDate selectedDate={selectedDate} />
+        <Search updateAppState={updateAppState} />
+        <ResetButton today={today} updateAppState={updateAppState} />
+      </div>
+      <div className={styles.col2}>
+        <LabelField label="Begins" message={"Jul 31"} />
+        <LabelField label="Ends" message={"Aug 29"} />
+        <LabelField label="Work Days" message={"22"} />
+        <LabelField label="Work Hours" message={"176"} />
+      </div>
+      <div className={styles.col3}>
+        <LabelField
+          label="Master Mail/Transfer"
+          message={formatSidebarDate(
+            getDateOfMasterTransfer(currentMonth, yearNumber)
+          )}
+        />
+        <LabelField
+          label="Master Payday"
+          message={formatSidebarDate(
+            getPriorWorkday(getDateOfMasterPayday(currentMonth, yearNumber))
+          )}
+        />
+        <LabelField
+          label="Release Master"
+          message={formatSidebarDate(
+            getDateOfMasterPayday(currentMonth, yearNumber)
+          )}
+        />
+        <LabelField
+          label="Master Issue"
+          message={formatSidebarDate(
+            getDateOfMasterPayroll(currentMonth, yearNumber)
+          )}
+        />
+        <LabelField
+          label="Direct Deposit"
+          message={formatSidebarDate(
+            getDateOfDirectDeposit(currentMonth, yearNumber)
+          )}
+        />
+      </div>
     </section>
   );
 };
