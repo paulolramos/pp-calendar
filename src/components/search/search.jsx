9e4f2c1225de9a5f48e5b389962bddc6e32f3510
Paulo Ramos <pauloramos64@gmail.com>
2019-08-09 16:50:01 -0700

    Search

---
+++ src/components/search/search.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Search.module.css";
+import { updateDate } from "../../BusinessLogic/functions";
+
+export const Search = ({ updateAppState }) => {
+  const onSubmit = e => {
+    e.preventDefault();
+    const value = new Date(document.getElementById("search").value);
+    const newDate = new Date(value.setDate(value.getDate() + 1));
+    updateAppState(state => updateDate(state)(newDate));
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="date" className={styles.label}>
+          Search by Date:
+        </label>
+        <input
+          id="search"
+          className={styles.input}
+          type="date"
+          autoComplete="off"
+        />
+        <button className={styles.button}>Go</button>
+      </form>
+    </div>
+  );
+};
