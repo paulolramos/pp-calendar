298b4cba1bfa1d3d86f57e9707f4134d73c372ed
Paulo Ramos <pauloramos64@gmail.com>
2019-08-09 16:49:10 -0700

    Calendar

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import ReactCalendar from "react-calendar";
-import { container } from "./calendar.module.css";
+import "./Calendar.css";
+import { updateDate } from "../../BusinessLogic/functions";
+import { UI } from "../../BusinessLogic/calendarUI";
 
-export const Calendar = () => {
+export const Calendar = ({
+  selectedDate,
+  yearNumber,
+  monthNumber,
+  currentMonth,
+  currentPattern,
+  updateAppState,
+}) => {
+  const onChange = changedDate =>
+    updateAppState(state => updateDate(state)(changedDate));
+
+  const selectFirstDayOfMonth = ({ activeStartDate }) =>
+    updateAppState(state => updateDate(state)(activeStartDate));
+
+  const addCalenderElements = ({ date, view }) => {
+    if (view === "month") {
+      return [
+        UI.renderHolidays(date, yearNumber),
+        UI.renderMasterCutoff(date, monthNumber, currentMonth, yearNumber),
+        UI.renderNoCycles(date, monthNumber, yearNumber),
+        UI.renderGreenCycles(date, monthNumber, yearNumber),
+        UI.renderBeginEndDates(date, currentPattern, currentMonth, yearNumber),
+        UI.renderPayday(date, currentMonth, yearNumber),
+        UI.renderDirectDeposit(date, currentMonth, yearNumber),
+      ].filter(itemToRender => itemToRender !== null);
+    } else {
+      return null;
+    }
+  };
+
   return (
-    <section className={container}>
-      <ReactCalendar />
+    <section className="container">
+      <ReactCalendar
+        value={selectedDate}
+        className="calendar"
+        onChange={onChange}
+        tileContent={addCalenderElements}
+        onActiveDateChange={selectFirstDayOfMonth}
+        calendarType="US"
+        showNeighboringMonth
+        minDetail="month"
+      />
     </section>
   );
 };
