19061f663d90251af2149ea8a2345d710721f0af
Paulo Ramos <pauloramos64@gmail.com>
2019-08-09 16:49:29 -0700

    ResetButton

---
+++ src/components/ResetButton/ResetButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./ResetButton.module.css";
+import { updateDate } from "../../BusinessLogic/functions";
+
+export const ResetButton = ({ today, updateAppState }) => {
+  return (
+    <button
+      className={styles.button}
+      onClick={() => updateAppState(state => updateDate(state)(today))}
+    >
+      Reset
+    </button>
+  );
+};
