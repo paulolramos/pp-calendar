8a8f0c57740ddc944fc0e81b845112e4a5c935a5
Paulo Ramos <PauloRamos64@gmail.com>
2019-08-08 23:00:34 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Dashboard } from "./components/dashboard/dashboard";
 
-function App() {
+export const App = () => {
+  const [appState] = useState({
+    today: new Date(),
+    selectedDate: new Date(),
+    yearInput: null,
+    // currentPattern: getPattern(new Date().getFullYear()),
+    currentMonth: new Date().toLocaleDateString("en-us", { month: "short" }),
+    monthNumber: new Date().getMonth() + 1,
+    yearNumber: new Date().getFullYear()
+  });
+
+  console.log("app state:", appState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/dashboard/dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./dashboard.module.css";
+
+export const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
