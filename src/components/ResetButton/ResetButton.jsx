b51737d36c9af7155e67b412d057d4c13ad5abad
Paulo Ramos <pauloramos64@gmail.com>
2019-08-22 16:19:58 -0700

    prevent reload when resetting

--- src/components/ResetButton/ResetButton.jsx
+++ src/components/ResetButton/ResetButton.jsx
@@ -3,11 +3,13 @@ import styles from "./ResetButton.module.css";
 import { updateDate } from "../../BusinessLogic/functions";
 
 export const ResetButton = ({ today, updateAppState }) => {
+  const handleClick = e => {
+    e.preventDefault();
+    updateAppState(state => updateDate(state)(today));
+  };
+
   return (
-    <button
-      className={styles.button}
-      onClick={() => updateAppState(state => updateDate(state)(today))}
-    >
+    <button className={styles.button} onClick={handleClick}>
       RESET
     </button>
   );
